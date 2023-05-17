82bdc9de6bbc453eaa99da66c0c2f522f63c7564
4charan4 <130393683+4charan4@users.noreply.github.com>
2023-05-17 09:12:53 +0530

    Add files via upload

---
+++ pythagoreantriplet.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter the smallest number:"))
+n2=int(input("Enter the 2nd smallest number:"))
+n3=int(input("Enter the largest number:"))
+if n1**2+n2**2==n3**2:
+    print(1)
+else:
+    print(0)
+    
+            
