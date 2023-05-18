3b07cb072980b3646a141bfcc8d6eea3b444cb12
4charan4 <130393683+4charan4@users.noreply.github.com>
2023-05-19 01:14:29 +0530

    Add files via upload

---
+++ input_until_-1.py
@@ -0,0 +1,25 @@
+l=[]
+e=[]
+o=[]
+i=0
+while True:
+    a=int(input("Enter a number:"))
+    if a!=-1:
+        if a%2==0:
+             e.append(a)
+        else:
+            o.append(a)
+        l.append(a)
+    else:
+        print("No.of even numbers:",len(e))
+        print("No.of odd numbers:",len(o))
+        break
+        
+
+            
+       
+        
+    
+    
+        
+    
