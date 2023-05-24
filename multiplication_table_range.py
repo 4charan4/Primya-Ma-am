34e0ee3554ba1563b6af464c393be6219f713ad6
4charan4 <130393683+4charan4@users.noreply.github.com>
2023-05-24 10:25:53 +0530

    Update multiplication_table_range.py

--- multiplication_table_range.py
+++ multiplication_table_range.py
@@ -1,8 +1,8 @@
 a=int(input("Start table:"))
 b=int(input("End table:"))
 c=int(input("End:"))
-for i in range(a,b+1):
-    for j in range(1,c+1):
-        print(i,"x",j,"=",i*j)
-        
+for i in range(1,c+1):
+    for j in range(a,b+1):
+        print(j,"x",i,"=",i*j,end="\t")
+    print(end="\n")
 
