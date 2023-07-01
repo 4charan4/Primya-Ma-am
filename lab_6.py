032c7fe996c1e96afcd6ec28af4b0865a66ea8e4
4charan4 <130393683+4charan4@users.noreply.github.com>
2023-07-01 23:23:22 +0530

    Update lab_6.py

--- lab_6.py
+++ lab_6.py
@@ -1,12 +1,12 @@
 dict1 = {}
 n=int(input("Enter number of elemnts:"))
 for i in range(n):
-    key = input("Enter employee's name: ")
-    val = input("Enter employee's salary: ")
+    key = input("Enter key: ")
+    val = input("Enter value: ")
     dict1[key] = val
 print(dict1)
 
 
 for key in dict1:
-    print(key,dict1[key])
+    print(key,":",dict1[key])
 
